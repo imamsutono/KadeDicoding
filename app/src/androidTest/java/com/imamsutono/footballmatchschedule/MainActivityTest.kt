46eb493ede4ea4b9ad1da4142fdf86ea6511a3e2
imamsutono <imamssutono@gmail.com>
2018-10-11 15:43:41 +0700

    Add comment to describe test step

--- app/src/androidTest/java/com/imamsutono/footballmatchschedule/MainActivityTest.kt
+++ app/src/androidTest/java/com/imamsutono/footballmatchschedule/MainActivityTest.kt
@@ -31,12 +31,14 @@ class MainActivityTest {
         Assert.assertEquals("com.imamsutono.footballmatchschedule", appContext.packageName)
     }
 
+    // test for swipe between tab is work correctly
     @Test
     fun swipePage() {
         onView(withId(viewpager_main)).check(matches(isDisplayed()))
         onView(withId(viewpager_main)).perform(swipeLeft())
     }
 
+    // check for tab layout is displayed
     @Test
     fun checkTabLayoutDisplayed() {
         onView(withId(tabs_main))
@@ -46,6 +48,7 @@ class MainActivityTest {
 
     @Test
     fun testAppBehaviour() {
+        // delay code execution
         sleep(DELAY)
         // check recycler view is displayed
         onView(withId(prev_match_list))
@@ -56,41 +59,51 @@ class MainActivityTest {
         onView(withId(prev_match_list)).perform(
                 RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(5)
         )
-        // click item in position 5 in recycler view
+        // click item in position 5 in recycler view, and go to detail activity
         onView(withId(prev_match_list)).perform(
                 RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(5, click())
         )
 
         sleep(DELAY)
+        // check home team badge image view is displayed
         onView(withId(home_badge))
                 .check(matches(isDisplayed()))
+        // check away team badge image view is displayed
         onView(withId(away_badge))
                 .check(matches(isDisplayed()))
 
+        // check add to favorite (star) menu is displayed
         onView(withId(add_to_favorite))
                 .check(matches(isDisplayed()))
+        // click add to favorite menu to add item to sqlite database
         onView(withId(add_to_favorite)).perform(click())
 
         sleep(DELAY)
+        // back from detail activity to main activity
         pressBack()
 
         sleep(DELAY)
+        // click view that contain FAVORITE text, in this case is favorite tab
         onView(withText("FAVORITE")).perform(click())
 
         sleep(DELAY)
+        // scroll to first item in favorite recycler view
         onView(withId(favorites_match_list)).perform(
                 RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(0)
         )
+        // click first item in favorite recycler view. item is added in past test
         onView(withId(favorites_match_list)).perform(
                 RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click())
         )
 
         sleep(DELAY)
+        // click add to favorite menu to removed item from sqlite database
         onView(withId(add_to_favorite)).perform(click())
 
         pressBack()
 
         sleep(DELAY)
+        // swipe down favorite tab to refresh recycler view and check that item is removed
         onView(withId(favorite_swipe_refresh)).perform(swipeDown())
 
         sleep(DELAY)
